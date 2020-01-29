530159cbd2cd66e490cb1161de8838f7d086f2f4
María Fernanda Gallo Cruz <mafergallocruz@gmail.com>
2020-01-28 22:51:06 -0600

    Solve my go-slices-maps
    Signed-off-by: María Fernanda Gallo Cruz <mafergallocruz@gmail.com>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	words := strings.Fields(s)
+	wordDict := make(map[string]int)
+
+	for _, s := range words {
+		//fmt.Println(s)
+		wordDict[s]++
+	}
+	return wordDict
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"code.google.com/p/go-tour/pic"
+)
+
+func Pic(dx, dy int) [][]uint8 {
+	// el := se usa si la inicializas
+	image := make([][]uint8, dx) //este es el slice
+	for x := 0; x < dx; x++ {
+		fila := make([]uint8, dy, dy)
+		for y := 0; y < dy; y++ {
+			// en cada fila se llenan las columnas con valores uint8
+			fila[y] = uint8((x + y) / 2)
+			fila[len(fila)-y-1] = fila[y] ^ uint8(x*y)
+		}
+		image[x] = fila
+	}
+	return image
+}
+
 func main() {
+	pic.Show(Pic)
 }
